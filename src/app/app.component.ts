7fe053e4bf252c7ac16af2ccc99db4b3a3624ae7
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-06-16 21:39:26 +0300

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,8 @@ export class AppComponent {
   constructor(private wikipedia: WikipediaService) { }
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe(pages => {
+      this.pages = pages;
     })
   }
 }
--- src/app/services/wikipedia.service.ts
+++ src/app/services/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +20,7 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   public search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
         format: 'json',
@@ -18,7 +29,9 @@ export class WikipediaService {
         srsearch: term,
         origin: '*'
       }
-    });
+    }).pipe(
+      pluck('query', 'search')
+    );
   }
 }
 
